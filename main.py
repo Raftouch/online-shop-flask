30129c5fc043a731ebebbd4605a9018dadfddeb0
raftouch <rafanelkin@gmail.com>
2025-05-30 13:37:16 +0200

    connect to db

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    isAvailable = db.Column(db.Boolean, default=True)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
